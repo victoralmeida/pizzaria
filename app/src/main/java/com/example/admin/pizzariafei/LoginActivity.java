a5b813062a313adbeeb03f6f5293ef44b2a1217f
Victor Almeida <victor.almeida@flux-it.com.br>
2017-11-21 11:21:38 -0200

    Commit teste, através do Android Studio

--- app/src/main/java/com/example/admin/pizzariafei/LoginActivity.java
+++ app/src/main/java/com/example/admin/pizzariafei/LoginActivity.java
@@ -3,12 +3,11 @@ package com.example.admin.pizzariafei;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class NovoPedidoActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_novo_pedido);
+        setContentView(R.layout.activity_login);
     }
-
 }
